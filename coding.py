2d44559f0fcc762ef7db05cfac32c66dd3b088dc
Magooch <crvallecito@gmail.com>
2024-08-22 09:55:14 -0600

    Add function that counts down from

--- coding.py
+++ coding.py
@@ -4,3 +4,15 @@ def pyramid():
         for j in range(i):
             print('*', end='')
         print()
+
+pyramid()
+
+# counts down to 0 from n
+def timer(n):
+    if not n:
+        return 0
+    while n >= 0:
+        print(n)
+        n -= 1
+    
+timer(10)
