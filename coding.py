acbb270914e8a420cdd2a74460f70bd9c6d759a1
Magooch <crvallecito@gmail.com>
2024-08-22 09:56:30 -0600

    Function that iterates through a list of integers and returns the even integers squared

--- coding.py
+++ coding.py
@@ -16,3 +16,12 @@ def timer(n):
         n -= 1
     
 timer(10)
+
+# takes a list of integers, singles out the evens and returns a new list of even squared integers
+def square_ints(lst):
+    if not lst:
+        return None
+    return [i * i for i in lst if i % 2 == 0]
+    
+lst_of_ints = [1, 2, 3, 4, 5, 6]
+print(square_ints(lst_of_ints))
