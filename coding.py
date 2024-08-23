bc3fa597fa9e06fe78ab84645ee4a6f9b6f8aba4
Magooch <crvallecito@gmail.com>
2024-08-23 13:18:49 -0600

    Add new unfinished code problem

--- coding.py
+++ coding.py
@@ -90,7 +90,6 @@ print(m_word_count(my_sentence))
 # iterates through a string and returns the number of vowels
 vowels = ["a", "e", "i", "o", "u"]
 
-# Write your function here
 def vowel_counter(string):
   vowel_count = 0
   for char in string:
@@ -99,3 +98,31 @@ def vowel_counter(string):
   return vowel_count
 
 print(vowel_counter("random string that should print 7"))
+
+# Friday August 23, 2024
+# unfinished code that is meant to run through a series of string methods on a list of poems, authors and years, eventually creating a string for each
+highlighted_poems = "Afterimages:Audre Lorde:1997,  The Shadow:William Carlos Williams:1915, Ecstasy:Gabriela Mistral:1925,   Georgia Dusk:Jean Toomer:1923,   Parting Before Daybreak:An Qi:2014, The Untold Want:Walt Whitman:1871, Mr. Grumpledump's Song:Shel Silverstein:2004, Angel Sound Mexico City:Carmen Boullosa:2013, In Love:Kamala Suraiyya:1965, Dream Variations:Langston Hughes:1994, Dreamwood:Adrienne Rich:1987"
+highlighted_poems_list = highlighted_poems.split(',')
+print(highlighted_poems_list)
+
+highlighted_poems_stripped = []
+for poem in highlighted_poems_list:
+  highlighted_poems_stripped.append(poem.strip())
+print(highlighted_poems_stripped)
+
+highlighted_poem_details = []
+for item in highlighted_poems_stripped:
+  highlighted_poem_details.append(item.split(':'))
+print(highlighted_poem_details)
+
+titles = []
+poets = []
+dates = []
+
+for item in highlighted_poem_details:
+  titles.append(item[0])
+  poets.append(item[1])
+  dates.append(item[2])
+
+for item in highlighted_poem_details:
+  print("The poem {} was published by {} in {}".format(titles, poets, dates))
