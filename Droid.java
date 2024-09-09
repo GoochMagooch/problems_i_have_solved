271864f665473f3be41fac3bc13ee3d5ba6daa58
Magooch <crvallecito@gmail.com>
2024-09-09 16:24:19 -0600

    Paste code for Droid project

---
+++ Droid.java
@@ -0,0 +1,24 @@
+public class Droid {
+    String name;
+    int batteryLevel = 100;
+  
+    public Droid(String droidName) {
+      String name = droidName;
+    }
+  
+    public String toString() {
+      return "Hello, I am a droid and my name is: " + name;
+    }
+  
+    public void performTask(String task) {
+      System.out.println(name + " is performing task: " + task);
+    }
+  
+    public static void main(String[] args) {
+      Droid codey = new Droid("Codey");
+      System.out.println(codey);
+      codey.performTask("fighting");
+      int batteryLevel = batteryLevel - 10;
+    }
+  
+  }
