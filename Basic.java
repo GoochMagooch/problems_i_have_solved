078b948d4c9ae97bc394b02cb32917e040d6ce91
Magooch <crvallecito@gmail.com>
2024-09-17 17:35:56 -0600

    Create source file for 'basic' program

---
+++ Basic.java
@@ -0,0 +1,7 @@
+public class Basic {
+
+    public static void main(String[] args) {
+	char[][] basic = {{'b', 'a', 's', 'i', 'c'}, {'b', 'a', 's', 'i', 'c'}, {'b', 'a', 's', 'i', 'c'}, {'b', 'a', 's', 'i', 'c'}, {'b', 'a', 's', 'i', 'c'}};
+    }
+
+}
