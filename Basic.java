c2f335fcfcad6f242c7bcd20afe3bcad32584beb
Magooch <crvallecito@gmail.com>
2024-09-28 17:45:44 -0600

    Add comments explaining the intended logic

--- Basic.java
+++ Basic.java
@@ -1,5 +1,11 @@
 public class Basic {
 
+	// a program that prints:
+	// B a s i c
+	// b A s i c
+	// b a S i c
+	// b a s I c
+	// b a s i C
     public static void main(String[] args) {
 	String[][] basic = {{"basic"}, {"basic"}, {"basic"}, {"basic"}, {"basic"}};
 	for (String i = 0; i < basic.length; i++) {
